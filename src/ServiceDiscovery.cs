f60802191fd96fea83a3fcd9dd897a0b2b3f3d5b
yushun-zhou <144811041+yushun-zhou@users.noreply.github.com>
2024-03-29 10:53:46 +0900

    Update ServiceDiscovery.cs

--- src/ServiceDiscovery.cs
+++ src/ServiceDiscovery.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Makaretu.Dns.Resolving;
+﻿using Makaretu.Dns.Resolving;
 using Microsoft.Extensions.Logging;
 
 namespace Makaretu.Dns;
